def3d8f437615a5d335652500fba40f9708ddf79
Felipe Costa <felipecostasiqu@gmail.com>
2025-07-07 10:26:48 -0300

    :bug: fix: ajust dark theme colors and alternate look dark theme button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import './index.css'
-import darkModeIcon from '../assets/images/dark-mode-icon.svg'
 import mainImage from '../assets/images/main-img.svg'
 
 /* Import Cards Web Images */
@@ -16,6 +15,9 @@ import avarusRexImage from '../assets/images/Cards/avarus-rex-image.png'
 import unityLightIcon from '../assets/images/icons/unity-light-mode.png'
 import mudblazorIcon from '../assets/images/icons/mudblazor.png'
 import csIcon from '../assets/images/icons/cs.png'
+import cssIcon from '../assets/images/icons/css3.png'
+import darkModeIcon from '../assets/images/icons/dark-mode-icon.svg'
+import lightModeIcon from '../assets/images/icons/light-mode-icon.png'
 
 
 
@@ -52,16 +54,16 @@ function App() {
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
                 <li className="nav-item">
-                  <a className="nav-link" href="#about-me">SOBRE</a>
+                <a className="nav-link" href="#about-me" style={{color: 'var(--bs-text-color)'}}>SOBRE</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#projects-web">PROJETOS</a>
+                  <a className="nav-link" href="#projects-web" style={{color: 'var(--bs-text-color)'}}>PROJETOS</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="#">CONTATO</a>
+                  <a className="nav-link" href="#" style={{color: 'var(--bs-text-color)'}}>CONTATO</a>
                 </li>
                 <li>
-                  <button className='button-dark-mode' onClick={toggleTheme}><img src={darkModeIcon} alt="Icon Dark Mode" /></button>
+                  <button className='button-dark-mode' onClick={toggleTheme}><img src={theme === 'dark' ? lightModeIcon : darkModeIcon} alt="Icon Dark Mode" /></button>
                 </li>
               </ul>
             </div>
@@ -72,8 +74,8 @@ function App() {
         <div className="container py-5 mt-5"> 
           <div className="row align-items-center g-5">
             <div className="col-md-7 pe-md-5">
-              <h1 className='display-3 blue-text font-electrolize fw-bold mb-3'>Desenvolvedor Web e Unity<span className='text-black'>.</span></h1>
-              <p className='h5 font-source-sans-3 fw-normal'>Olá, meu nome é Felipe. Sou um estudante, estou focado em aprender <span className='blue-text fw-bold'>C#</span>,<br /> <span className='blue-text fw-bold'>Unity</span> e o ecossistema <span className='blue-text fw-bold'>.NET</span></p>
+              <h1 className='display-3 blue-text font-electrolize fw-bold mb-3'>Desenvolvedor Web e Unity<span style={{color: 'var(--bs-text-color)'}}>.</span></h1>
+              <p className='h5 font-source-sans-3 fw-normal ' style={{color: 'var(--bs-text-color)'}}>Olá, meu nome é Felipe. Sou um estudante, estou focado em aprender <span className='blue-text fw-bold'>C#</span>,<br /> <span className='blue-text fw-bold'>Unity</span> e o ecossistema <span className='blue-text fw-bold'>.NET</span></p>
               <p className='h5 blue-text font-orbitron fw-bold mt-4'>Estou sempre<br></br> buscando aprender.</p>
             </div>
             <div className="col-md-5 text-center">
@@ -84,7 +86,7 @@ function App() {
       </main>
         <section className="container-fluid py-4" style={{ minHeight: '43rem', width: '100%' }}>
           <div className="container">
-            <h1 id='about-me' className='fw-bold'>SOBRE MIM</h1>
+            <h1 id='about-me' className='fw-bold' style={{color: 'var(--bs-text-color)'}}>SOBRE MIM</h1>
             <hr 
               style={{ 
                 width: '60px', 
@@ -98,8 +100,8 @@ function App() {
             />
             <div className="row d-flex align-items-start mt-5">
               <div className="col-md-6 mb-4 mb-md-0 pe-md-5">
-                <h1 className="mb-3 fw-bold">ME CONHEÇA</h1>
-                <p className='h5 mt-3'>
+                <h1 className="mb-3 fw-bold" style={{color: 'var(--bs-text-color)'}}>ME CONHEÇA</h1>
+                <p className='h5 mt-3' style={{color: 'var(--bs-text-color)'}}>
                   Olá! Meu nome é <strong>Felipe</strong>. Tenho 17 anos e moro em São Paulo. Desde cedo, sempre fui engajado por <strong>tecnologia</strong> e <strong>programação</strong>. <br /> <br />
 
                   Atualmente, estou focado no estudo do ecossistema <strong>.NET</strong>, explorando as possibilidades que ele oferece para o desenvolvimento de inúmeras aplicações. <br /> <br />
@@ -108,10 +110,10 @@ function App() {
                 </p>
               </div>
               <div className="col-md-6 ps-md-5">
-                <h1 className="mb-3 fw-bold">TECNOLOGIAS</h1>
+                <h1 className="mb-3 fw-bold" style={{color: 'var(--bs-text-color)'}}>TECNOLOGIAS</h1>
                 <div className="d-flex flex-wrap gap-3" id='tech'>
                     <img src="https://cdn.simpleicons.org/html5/E34F26" alt="HTML5" width="40"/>
-                    <img src="https://cdn.simpleicons.org/css3/1572B6" alt="CSS3" width="40"/>
+                    <img src={cssIcon} alt="CSS3" width="40"/>
                     <img src="https://cdn.simpleicons.org/javascript/F7DF1E" alt="JavaScript" width="40" />
                     <img src="https://cdn.simpleicons.org/typescript/3178C6" alt="TypeScript" width="40" />
                     <img src="https://cdn.simpleicons.org/react/61DAFB" alt="React" width="40" />
@@ -126,7 +128,7 @@ function App() {
           </div>
         </section>
         <section className="container py-5">
-          <h1 id="projects-web" className="fw-bold text-center mb-5">PROJETOS WEB</h1>
+          <h1 id="projects-web" className="fw-bold text-center mb-5" style={{color: 'var(--bs-text-color)'}}>PROJETOS WEB</h1>
             <hr 
               style={{ 
                 width: '60px', 
@@ -157,7 +159,7 @@ function App() {
                       HTML5
                     </span>
                     <span className="d-flex align-items-center gap-1 badge rounded-pill text-bg-dark">
-                      <img src="https://cdn.simpleicons.org/css3/1572B6" width="16" alt="CSS3" />
+                      <img src={cssIcon} width="16" alt="CSS3" />
                       CSS3
                     </span>
                     <span className="d-flex align-items-center gap-1 badge rounded-pill text-bg-dark">
@@ -191,7 +193,7 @@ function App() {
                       HTML5
                     </span>
                     <span className="d-flex align-items-center gap-1 badge rounded-pill text-bg-dark">
-                    <img src="https://cdn.simpleicons.org/css3/1572B6" width="16" alt="CSS3" />
+                    <img src={cssIcon} width="16" alt="CSS3" />
                       CSS3
                     </span>
                     <span className="d-flex align-items-center gap-1 badge rounded-pill text-bg-dark">
@@ -230,7 +232,7 @@ function App() {
                       HTML5
                     </span>
                     <span className="d-flex align-items-center gap-1 badge rounded-pill text-bg-dark">
-                      <img src="https://cdn.simpleicons.org/css3/1572B6" width="16" alt="CSS3" />
+                      <img src={cssIcon} width="16" alt="CSS3" />
                       CSS3
                     </span>
                     <span className="d-flex align-items-center gap-1 badge rounded-pill text-bg-dark">
@@ -253,7 +255,7 @@ function App() {
           </div>
         </section>
         <section className="container py-5">
-          <h1 id="projects-web" className="fw-bold text-center mb-5">PROJETOS UNITY</h1>
+          <h1 id="projects-web" className="fw-bold text-center mb-5" style={{color: 'var(--bs-text-color)'}}>PROJETOS UNITY</h1>
             <hr 
               style={{ 
                 width: '60px', 
